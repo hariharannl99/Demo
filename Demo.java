123a33eee638fff0d8f408e31353ad9f26cc01e0
HARIHARAN LATHA NAGASUBRAMANIAN <128220711+hariharannl99@users.noreply.github.com>
2024-10-22 22:32:44 +0530

    number value changed from 10 to 100

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 Public class Demo{
     Public static void main(String args[]){
-        int number = 10;
+        int number = 100;
         char letter = 'a';
         System.out.println(number+ " " + letter);
     }
