6fadceb087975d6c52bc9c55b73b4dcfdb685ccd
Hariharan Latha Nagasubrmanian <hariharannagu12@gmail.com>
2024-10-22 22:39:46 +0530

    value in local changed from 10 to 1000

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-Public class Demo{
-    Public static void main(String args[]){
-        int number = 10;
+public class Demo{
+    public static void main(String args[]){
+        int number = 1000;
         char letter = 'a';
         System.out.println(number+ " " + letter);
     }
